61ef874a7cf0fb5c69e02fcee8b7ee9a17acc23d
nick Latham <nlatham@zagmail.gonzaga.edu>
2021-04-01 17:01:39 -0700

    feat: dynamically creates pages for each mountain

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import MountainDetails from './MountainDetails';
+import { NavLink, Switch, Route, BrowserRouter } from 'react-router-dom';
+import './MountainDetails';
 
-function App() {
+const App = () => {
+
+  const mtnNames = mountainNames();
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={MainPage}></Route>
+          {mtnNames.map((mtn, i) => (
+            <Route exact path={"/"+mtn["name"]} component={props => <MountainDetails mountainName={mtn["name"]}/>}></Route>
+          ))}
+        </Switch>
+      </BrowserRouter>
+  );
+  
+
+}
+
+const MainPage = () => {
+  return (
+    <div>
+      Main Page
     </div>
   );
 }
+
+function mountainNames () {
+  return [
+    {"name": "lookout", "lat": 0, "lng": 0},
+    {"name": "schweitzer", "lat": 0, "lng": 0},
+    {"name": "silver_mountain", "lat": 0, "lng": 0},
+  ]
+}
+
+
 
 export default App;
---
+++ src/MountainDetails.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MountainDetails = ({mountainName}) => {
+    return (
+        <div>Mountain Details {mountainName}</div>
+    );
+}
+
+export default MountainDetails;
