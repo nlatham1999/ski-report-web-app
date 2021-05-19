103e9873ad0347d5aff16f62051fbe05231988f0
nick Latham <nlatham@zagmail.gonzaga.edu>
2021-05-19 11:23:22 -0700

    fix: updating list of names

--- src/MountainNames.js
+++ src/MountainNames.js
@@ -8,9 +8,12 @@ function mountainNames () {
         {"name": "49degrees_north", "lat": 48.301204234032795, "lng": -117.56266933896624, "display name": "49° North", "state": "WA"},
         {"name": "mount_spokane", "lat": 47.923826183676, "lng": -117.11277380560337, "display name": "Mount Spokane", "state": "WA"},
         {"name": "whitefish", "lat": 48.480740487490934, "lng": -114.35014905615843, "display name": "Whitefish", "state": "MT"},
-        {"name": "alpine_meadows", "lat": 39.16476641125912, "lng": -120.23860027188792, "display name": "Alpine Meadows", "state": "CA"},
-        {"name": "big_bear_mountain", "lat": 34.228670112278024, "lng": -116.85781021020217, "display name": "Big Bear Mountain", "state": "CA"},
-        {"name": "donner_ski_ranch", "lat": 39.31788128659341, "lng": -120.33026492955482, "display name": "Donner Ski Ranch", "state": "CA"}
+        {"name": "mount_hood", "lat": 45.374006027946784, "lng": -121.69620863013814, "display name": "Mount Hood", "state": "OR"},
+        {"name": "mammoth_mountain", "lat": 37.65147263379701, "lng": -119.02623317738863, "display name": "Mammoth Mountain", "state": "CA"},
+        {"name": "mission_ridge", "lat": 47.31512546744439, "lng": -120.20051466081155, "display name": "Mission Ridge", "state": "WA"}
+        // {"name": "alpine_meadows", "lat": 39.16476641125912, "lng": -120.23860027188792, "display name": "Alpine Meadows", "state": "CA"},
+        // {"name": "big_bear_mountain", "lat": 34.228670112278024, "lng": -116.85781021020217, "display name": "Big Bear Mountain", "state": "CA"},
+        // {"name": "donner_ski_ranch", "lat": 39.31788128659341, "lng": -120.33026492955482, "display name": "Donner Ski Ranch", "state": "CA"}
     ]
 }
 
