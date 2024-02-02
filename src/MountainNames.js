7abef07a2818acd7e5795b3111c2cbe707ed4543
nlatham1999 <nicholaslatham1999@gmail.com>
2024-02-02 12:38:41 -0800

    changing coords

--- src/MountainNames.js
+++ src/MountainNames.js
@@ -4,7 +4,7 @@ function mountainNames () {
     return [
         {"name": "lookout", "lat": 47.45611828584227, "lng": -115.69710169853091, "display name": "Lookout Pass", "state": "ID"},
         {"name": "schweitzer", "lat": 48.36820376316482, "lng": -116.62277628870922, "display name": "Schweitzer Mountain", "state": "ID"},
-        {"name": "silver_mountain", "lat": 47.49906915722505, "lng": -116.11885721016526, "display name": "Silver Mountain", "state": "ID"},
+        {"name": "silver_mountain", "lat": 47.45383421426021, "lng": -115.67010874433107, "display name": "Silver Mountain", "state": "ID"},
         {"name": "49degrees_north", "lat": 48.301204234032795, "lng": -117.56266933896624, "display name": "49° North", "state": "WA"},
         {"name": "mount_spokane", "lat": 47.923826183676, "lng": -117.11277380560337, "display name": "Mount Spokane", "state": "WA"},
         {"name": "whitefish", "lat": 48.480740487490934, "lng": -114.35014905615843, "display name": "Whitefish", "state": "MT"},
